d6741c4486ce6568ef3ca2ea169a96eea72c528f
ReineCarole <carolebalep@outlook.com>
2025-01-24 04:06:52 +0100

    Navbar and Side Menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,35 @@
 
 import { CldImage } from "next-cloudinary";
 import { CldUploadButton } from "next-cloudinary";
+import { useState } from "react";
+
+type UploadResult = {
+  info: {
+    public_id: string;
+  };
+  event: "success";
+};
 
 export default function Home() {
+  const [imageId, setImageId] = useState("");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <CldUploadButton
-          onUpload={(result: any) => {
-            console.log(result);
+          onUploadAdded={(result: UploadResult) => {
+            setImageId(result.info.public_id);
           }}
           uploadPreset="giomsr4s"
         />
-
-        <CldImage
-          width="960"
-          height="600"
-          src="samples/animals/kitten-playing"
-          sizes="100vw"
-          alt="Description of my image"
-        />
+        {imageId && (
+          <CldImage
+            width="960"
+            height="600"
+            src={imageId}
+            sizes="100vw"
+            alt="Description of my image"
+          />
+        )}
       </main>
     </div>
   );
