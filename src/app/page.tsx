a5903f6e82b33b7904cf60c7abe35122351e2fd6
ReineCarole <carolebalep@outlook.com>
2025-02-02 05:43:34 +0100

    Working on Gallery Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,11 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <CldUploadButton
-          onUploadAdded={(result: UploadResult) => {
-            setImageId(result.info.public_id);
+          onUploadAdded={(result: any) => {
+            const uploadResult = result as UploadResult;
+            if (uploadResult.info?.public_id) {
+              setImageId(uploadResult.info.public_id);
+            }
           }}
           uploadPreset="giomsr4s"
         />
